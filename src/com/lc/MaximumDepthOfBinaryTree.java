dcbf779ae9b2e91ec9ac35de65cc8e13151f8df5
vinayakmarali <vinayakmarali@Gotcha.local>
2019-01-17 01:48:23 -0600

    Maximum Depth of Binary Tree

---
+++ src/com/lc/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,13 @@
+package com.lc;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return Math.max(left_height, right_height) + 1;
+        }
+    }
+}
