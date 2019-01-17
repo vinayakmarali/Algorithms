0cdd2540e3c41183ec1e5459909697967bba7f6d
vinayakmarali <vinayakmarali@Gotcha.local>
2019-01-17 00:33:30 -0600

    Same Tree

---
+++ src/com/lc/SameTree.java
@@ -0,0 +1,23 @@
+package com.lc;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        if(p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
