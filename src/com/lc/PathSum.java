db2262a9227714f2259f5549358bd234c83c6989
vinayakmarali <vinayakmarali@Gotcha.local>
2019-01-18 01:43:31 -0600

    Path Sum

---
+++ src/com/lc/PathSum.java
@@ -0,0 +1,14 @@
+package com.lc;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+        sum = sum - root.val;
+        if((root.left == null) && (root.right == null)) {
+            return (sum == 0);
+        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
