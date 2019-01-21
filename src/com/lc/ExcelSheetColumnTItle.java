89a53b659e430d33daebc45e856521c4e063aa73
vinayakmarali <vinayakmarali@Gotcha.local>
2019-01-20 21:49:54 -0600

    Excel Sheet Column Title

---
+++ src/com/lc/ExcelSheetColumnTItle.java
@@ -0,0 +1,14 @@
+package com.lc;
+
+public class ExcelSheetColumnTItle {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            n--;
+            sb.insert(0, (char) ('A' + n % 26));
+            n = n / 26;
+        }
+        return sb.toString();
+    }
+}
